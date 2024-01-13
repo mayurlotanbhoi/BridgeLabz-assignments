8659707126dad5ec9c5dfd2ad6182970660744d4
mayur bhoi <mayurbhoi200@gmail.com>
2024-01-13 09:27:44 +0530

    solve Day 5 assingment

---
+++ Day5PrimeAndFibonach/CheackPrimeNum.java
@@ -0,0 +1,28 @@
+
+public class CheackPrimeNum {
+
+    public static boolean findPrimeNum(int num) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int count = 0;
+        int num = 2;
+
+        while (count < 10) {
+            boolean isPrime = findPrimeNum(num);
+            if (isPrime) {
+                System.out.println(num);
+                count++;
+            }
+            num++;
+
+        }
+    }
+}
