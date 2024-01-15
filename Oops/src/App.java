a9952488f04cb51489af01051ba872c41083bb26
mayur bhoi <mayurbhoi200@gmail.com>
2024-01-15 09:23:19 +0530

    classes and objects

--- Oops/src/App.java
+++ Oops/src/App.java
@@ -1,5 +1,10 @@
+import classandObjExp.Car;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Car tata = new Car();
+
+        tata.startCar(" manual ");
     }
 }
---
+++ Oops/src/classandObjExp/Car.java
@@ -0,0 +1,32 @@
+package classandObjExp;
+
+public class Car {
+
+    // this is Attribute or instance variable
+    int star, wheel, price;
+    String brand, model;
+
+    // this is default constructor
+    public Car() {
+
+    }
+
+    // paramiterize costructor
+    public Car(int star, int wheel, int price, String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+        this.star = star;
+        this.wheel = wheel;
+        this.price = price;
+    }
+
+    // this is method or behevoir of class
+    public void startCar() {
+        System.out.println("Car starting");
+    }
+
+    public void startCar(String methodOfStart) {
+        System.out.println("Car starting method" + methodOfStart);
+    }
+
+}
